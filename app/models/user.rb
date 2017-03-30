ee1049963c4ce1636fa1622ded6aa0c779bf16c7
Bijjou <mbijou@live.de>
2017-03-30 22:58:34 +0000

    aws paperclip9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ActiveRecord::Base
     has_one :employee
     has_attached_file :image, styles: { medium: "300x300>", small: "200x200", thumb: "100x100>" }
-    :storage => :s3,
-                    :bucket => 'wawilkw',
-                    :s3_credentials => "#{Rails.root}/config/aws.yml",
-                    :path => "resources/:id/:style/:basename.:extension"
-                    :url => ':s3_domain_url'
-
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
