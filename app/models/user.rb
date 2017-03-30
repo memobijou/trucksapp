4e8ec586ff4ea09a75eb84a8d0e69f30ad5b84fd
Bijjou <mbijou@live.de>
2017-03-30 22:21:25 +0000

    aws paperclip5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
     has_one :employee
-    has_attached_file :image, styles: { medium: "300x300>", small: "200x200", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    has_attached_file :image, styles: { medium: "300x300>", small: "200x200", thumb: "100x100>" }
 end
